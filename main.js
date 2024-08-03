8077b470d4833299e5eb2ec8a522da7dd35c293b
Widodo <widodroid@gmail.com>
2024-08-04 01:24:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ form.onsubmit = async (ev) => {
         role: "user",
         parts: [
           //{ inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-          { text:`${promptInput.value} , gunakan bahasa gaul, lucu dan berikan roasting singkat, lucu, menyindir di akhir kalimat tanpa menyertakan kata kunci`, },
+          { text:promptInput.value},
         ],
       },
     ];
